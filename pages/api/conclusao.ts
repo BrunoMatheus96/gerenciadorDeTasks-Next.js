f421b611b8eb6095bf21aafd19dbbd859a8fc182
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2024-03-08 10:02:19 -0300

    Finalização da API PATCH de conclusão

--- pages/api/conclusao.ts
+++ pages/api/conclusao.ts
@@ -3,6 +3,7 @@ import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { TarefaModel } from "@/models/TarefaModel";
 import { UsuarioModel } from "@/models/UsuarioModel";
 import { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
+import { ConclusapRequisicao } from "@/types/conclusaoRequisicao";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const endpointConclusao = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
@@ -14,17 +15,36 @@ const endpointConclusao = async (req: NextApiRequest, res: NextApiResponse<Respo
 
             if (!tarefaPorId) {
                 return res.status(400).json({ erro: "É preciso selecionar uma tarefa" });
+            } else {
+
+                //Se não tiver tarefa regitrada ou essa tarefa não for do o Usuário logado
+                if (tarefaPorId.idUsuario != userId) //idUsuário do banco Tarefas é diferente do ID do usuário logado no banco de usuários
+                {
+                    return res.status(404).json({ erro: "Tarefa não encontrada para esse usuário" });
+                }
+
+                const { concluido } = req.body as ConclusapRequisicao;
+
+                const statusAtual = tarefaPorId.conclusao;
+
+                if (statusAtual === false && concluido === false) {
+                    return res.status(400).json({ erro: "Essa tarefa não pode ser reaberta, pois ela não está concluída" });
+                }
+
+                if (statusAtual === true && concluido === true) {
+                    return res.status(400).json({ erro: "Essa tarefa já foi concluída" });
+                }
+
+                // Busca a tarefa pelo ID no banco de dados
+                const atualizarTarefa = await TarefaModel.findByIdAndUpdate(
+                    id, // ID da tarefa a ser atualizada
+                    { conclusao: concluido }, // Novos dados a serem substituídos
+                    { new: true } // Para retornar o documento atualizado
+                );
+
+                //await TarefaModel.updateOne(atualizacaoASerSalva); // O método create cria o objeto definido em tarefaASerSalvo na Coleção
+                return res.status(200).json(atualizarTarefa);
             }
-
-            //Se não tiver tarefa regitrada ou essa tarefa não for do o Usuário logado
-            if (tarefaPorId.idUsuario != userId) //idUsuário do banco Tarefas é diferente do ID do usuário logado no banco de usuários
-            {
-                return res.status(404).json({ erro: "Tarefa não encontrada para esse usuário" });
-            }
-
-            const {concluido} = req.body;
-
-            return res.status(200).json(tarefaPorId);
         }
         console.log("Foi requisitado um " + req.method + ` mas correto é um PATCH`);
         return res.status(405).json({ erro: "Método informado não é válido" });
