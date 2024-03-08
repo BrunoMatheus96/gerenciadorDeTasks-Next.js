f50951e99df7c8445f3c5a48b0acb07d1abee428
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2024-03-08 18:22:20 -0300

    conclusão da exclusão de tarefas

---
+++ pages/api/exclusao.ts
@@ -0,0 +1,41 @@
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { TarefaModel } from "@/models/TarefaModel";
+import { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
+import { ms } from "date-fns/locale";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+const endpointExclusao = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+        if (req.method === "DELETE") {
+
+            const { userId, id } = req.query;
+            const tarefaPorId = await TarefaModel.findById(id); // Procura no banco o ID da tarefa para o ID logado
+
+            if (!id) {
+                return res.status(400).json({ erro: `É preciso informar um ID` });
+
+            } else if (!tarefaPorId || tarefaPorId.idUsuario != userId) //idUsuário do banco Tarefas é diferente do ID do usuário logado no banco de usuários
+            {
+                return res.status(404).json({ erro: "Tarefa não encontrada para esse usuário" });
+
+            } else {
+
+                await TarefaModel.findByIdAndDelete(id);
+
+                return res.status(200).json({ msg: `Tarefa deletada com sucesso` });
+
+            }
+
+        }
+        console.log("Foi requisitado um " + req.method + ` mas correto é um DELETE`);
+        return res.status(405).json({ erro: "Método informado não é válido" });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ erro: "Erro ao cadastrar tarefa. Tente novamente mais tarde" });
+    }
+}
+
+/*Essa linha faz com que quando o if for chamado, ele primeiro vai tentar se conectar ao banco de dados e depois executar o endpoint (if)*/
+export default validarTokenJWT(conectarMongoDB(endpointExclusao));
